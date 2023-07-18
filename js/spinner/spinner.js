2f3316beccd2364f249fa2dc49e41b51ade8caba
Sergey Petukhov <banny75@i.ua>
2023-07-18 22:12:34 +0300

    flowers
    fixed

--- js/spinner/spinner.js
+++ js/spinner/spinner.js
@@ -15,24 +15,3 @@ window.addEventListener('load', function() {
     spinnerContainer.style.display = 'none'; // hidden spinner
     content.style.display = 'block';
 });
-
-
-// window.addEventListener('load', function() {
-//     const spinner = new Spinner({
-//         lines: 12,
-//         length: 50,
-//         width: 6,
-//         radius: 30,
-//         color: 'red',
-//         speed: 1,
-//     }).spin();
-
-//     const spinnerContainer = document.getElementById('spinner');
-//     spinnerContainer.appendChild(spinner.el);
-
-//     setTimeout(function() {
-//         const content = document.getElementById('content');
-//         spinnerContainer.style.display = 'none'; // hidden spinner
-//         content.style.display = 'block';
-//     }, 3000); // 3 second delay
-// });
